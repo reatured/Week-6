f093d0909dbb0b78efc826298d2ddc15e09fbd42
reatured <61943376+reatured@users.noreply.github.com>
2021-05-20 17:23:34 -0400

    Update jump pad and gate animation

---
+++ Assets/Scripts/JumpPadBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadBehavior : MonoBehaviour
+{
+
+    public float jumpForce;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            Rigidbody playerRb = other.GetComponent<Rigidbody>();
+
+            playerRb.AddForce(other.transform.up * jumpForce, ForceMode.Impulse);
+
+            Debug.Log("Jump");
+        }
+    }
+}
